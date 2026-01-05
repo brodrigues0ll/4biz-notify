cc64b5ffcf6c2e45d67f41dc203b2d2005fc90eb
brodrigues0ll <bernardorg99@hotmail.com>
2026-01-05 14:40:04 -0300

    migrate to shadcn + add mobile first

--- components/Providers.js
+++ components/Providers.js
@@ -1,11 +1,13 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function Providers({ children, session }) {
   return (
     <SessionProvider session={session}>
       {children}
+      <Toaster position="bottom-center" richColors closeButton />
     </SessionProvider>
   );
 }
